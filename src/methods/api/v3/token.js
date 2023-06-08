f90b624add565e716abc3c3e9a43f09c852b61eb
Esinko <34211401+Esinko@users.noreply.github.com>
2023-06-08 20:34:38 +0300

    fix: i forgor if in v3

--- src/methods/api/v3/token.js
+++ src/methods/api/v3/token.js
@@ -21,133 +21,127 @@ module.exports = {
      * @param {function} next
      */
     handler: async (req, res, next) => {
-        const url = new URL(req.url, `http://${req.headers.host}`)
 
         // Handle POST requests
         if (req.method === "POST") {
-            if (url.pathname === "/api/v1/token") { // Exchange code for token
-                // Get input params
-                if (req.headers["content-type"] !== "application/x-www-form-urlencoded") {
-                    res.writeHead(400, {
-                        "Content-Type": "application/json"
-                    })
-                    return res.end(JSON.stringify({
-                        error: "Invalid content-type header"
-                    }))
-                }
-
-                // Get the request body
-                const body = await getBody(req)
-
-                // Parse params
-                const requestParams = parseFormUrlEncoded(body)
-
-                if (!requestParams) {
-                    res.writeHead(400, {
-                        "Content-Type": "application/json"
-                    })
-                    return res.end(JSON.stringify({
-                        error: "Invalid form body"
-                    }))
-                }
-
-                // Get all given inputs
-                const code = requestParams.get("code") ?? ""
-                const grantType = requestParams.get("grant_type") ?? ""
-                const redirectURI = requestParams.get("redirect_uri") ?? ""
-                const clientId = requestParams.get("client_id") ?? ""
-                const clientSecret = requestParams.get("client_secret") ?? ""
-
-                // Make sure we got everything
-                const requiredKeys = ["code", "grant_type", "redirect_uri", "client_id", "client_secret"]
-                if (requiredKeys.map((key) => requestParams.has(key)).includes(false)) {
-                    res.writeHead(400, {
-                        "Content-Type": "application/json"
-                    })
-                    return res.end(JSON.stringify({
-                        error: `Missing or empty query parameter(s): ${requiredKeys.filter((key) => !requestParams.has(key))}`
-                    }))
-                }
-
-                // Validate grant type
-                if (grantType !== "authorization_code") {
-                    res.writeHead(400, {
-                        "Content-Type": "application/json"
-                    })
-                    return res.end(JSON.stringify({
-                        error: "\"grant_type\" must be \"authorization_code\""
-                    }))
-                }
-
-                // Process the token request
-                try {
-                    const application = await getApplication(clientId)
-
-                    // Make sure the application credentials are valid
-                    if (application === null || application.secret !== clientSecret) {
-                        res.writeHead(401, {
-                            "Content-Type": "application/json"
-                        })
-                        return res.end(JSON.stringify({
-                            error: "Invalid client credentials"
-                        }))
-                    }
-
-                    // Make sure the redirect url is valid
-                    if (!application.redirectURLs.includes(redirectURI)) {
-                        res.writeHead(401, {
-                            "Content-Type": "application/json"
-                        })
-                        return res.end(JSON.stringify({
-                            error: "Invalid redirect_uri"
-                        }))
-                    }
-
-                    // Get authentication session
-                    const session = await getAuthenticationSession({ code })
-                    if (!session) {
-                        res.writeHead(404, {
-                            "Content-Type": "application/json"
-                        })
-                        return res.end(JSON.stringify({
-                            error: "Invalid or expired code"
-                        }))
-                    }
-
-                    // Make sure we are in the desired stage
-                    if (session.status !== "completed") {
-                        res.writeHead(404, {
-                            "Content-Type": "application/json"
-                        })
-                        return res.end(JSON.stringify({
-                            error: "Invalid or expired authentication session"
-                        }))
-                    }
-
-                    // Update session details
-                    const token = generateRandomString(32)
-                    await updateAuthenticationSession({ code }, {
-                        status: "stored",
-                        timestamp: new Date().getTime(),
-                        token
-                    })
-
-                    // Return the token
-                    res.writeHead(200, {
-                        "Content-Type": "application/json"
-                    })
-                    return res.end(JSON.stringify({
-                        access_token: token, // Wow. All we needed.
-                        expiry: expiryForStatuses.stored
-                    }))
-                } catch (e) {
-                    console.error("Token", e)
-                    return handleError(e, res)
-                }
+            // Get input params
+            if (req.headers["content-type"] !== "application/x-www-form-urlencoded") {
+                res.writeHead(400, {
+                    "Content-Type": "application/json"
+                })
+                return res.end(JSON.stringify({
+                    error: "Invalid content-type header"
+                }))
             }
 
-            // No method matches this POST request
-            return next()
+            // Get the request body
+            const body = await getBody(req)
+
+            // Parse params
+            const requestParams = parseFormUrlEncoded(body)
+
+            if (!requestParams) {
+                res.writeHead(400, {
+                    "Content-Type": "application/json"
+                })
+                return res.end(JSON.stringify({
+                    error: "Invalid form body"
+                }))
+            }
+
+            // Get all given inputs
+            const code = requestParams.get("code") ?? ""
+            const grantType = requestParams.get("grant_type") ?? ""
+            const redirectURI = requestParams.get("redirect_uri") ?? ""
+            const clientId = requestParams.get("client_id") ?? ""
+            const clientSecret = requestParams.get("client_secret") ?? ""
+
+            // Make sure we got everything
+            const requiredKeys = ["code", "grant_type", "redirect_uri", "client_id", "client_secret"]
+            if (requiredKeys.map((key) => requestParams.has(key)).includes(false)) {
+                res.writeHead(400, {
+                    "Content-Type": "application/json"
+                })
+                return res.end(JSON.stringify({
+                    error: `Missing or empty query parameter(s): ${requiredKeys.filter((key) => !requestParams.has(key))}`
+                }))
+            }
+
+            // Validate grant type
+            if (grantType !== "authorization_code") {
+                res.writeHead(400, {
+                    "Content-Type": "application/json"
+                })
+                return res.end(JSON.stringify({
+                    error: "\"grant_type\" must be \"authorization_code\""
+                }))
+            }
+
+            // Process the token request
+            try {
+                const application = await getApplication(clientId)
+
+                // Make sure the application credentials are valid
+                if (application === null || application.secret !== clientSecret) {
+                    res.writeHead(401, {
+                        "Content-Type": "application/json"
+                    })
+                    return res.end(JSON.stringify({
+                        error: "Invalid client credentials"
+                    }))
+                }
+
+                // Make sure the redirect url is valid
+                if (!application.redirectURLs.includes(redirectURI)) {
+                    res.writeHead(401, {
+                        "Content-Type": "application/json"
+                    })
+                    return res.end(JSON.stringify({
+                        error: "Invalid redirect_uri"
+                    }))
+                }
+
+                // Get authentication session
+                const session = await getAuthenticationSession({ code })
+                if (!session) {
+                    res.writeHead(404, {
+                        "Content-Type": "application/json"
+                    })
+                    return res.end(JSON.stringify({
+                        error: "Invalid or expired code"
+                    }))
+                }
+
+                // Make sure we are in the desired stage
+                if (session.status !== "completed") {
+                    res.writeHead(404, {
+                        "Content-Type": "application/json"
+                    })
+                    return res.end(JSON.stringify({
+                        error: "Invalid or expired authentication session"
+                    }))
+                }
+
+                // Update session details
+                const token = generateRandomString(32)
+                await updateAuthenticationSession({ code }, {
+                    status: "stored",
+                    timestamp: new Date().getTime(),
+                    token
+                })
+
+                // Return the token
+                res.writeHead(200, {
+                    "Content-Type": "application/json"
+                })
+                return res.end(JSON.stringify({
+                    access_token: token, // Wow. All we needed.
+                    expiry: expiryForStatuses.stored
+                }))
+            } catch (e) {
+                console.error("Token", e)
+                return handleError(e, res)
+            }
         }
 
         // Handle POST requests
